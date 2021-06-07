a2de76c9861d6d5f32bc394c434c522e8650e03f
VulpesPaw <richard.jh.johansson@dev.com>
2021-06-07 12:59:09 +0200

    Settings bug fixed
    Settings bug was faulty paths on startup

--- Gomoku/SettingsForm.cs
+++ Gomoku/SettingsForm.cs
@@ -15,31 +15,36 @@ namespace Gomoku
     {
         private string @fullPath;
         public string gameTag, darkmode;
-        private string @dirPath, filename;
+        public string @dirPath, filename;
         private FileIO fileHandler = new FileIO();
 
         public SettingsForm(string @_dirPath, string _filename)
         {
-            InitializeComponent();
-            this.@fullPath = Path.Combine(_dirPath, _filename);
-            this.dirPath = _dirPath;
-            this.filename = _filename;
-
-            // Applies inital settings if present
-            bool dirExist = fileHandler.checkForDir(fullPath);
-            if(dirExist)
+            try
             {
-                string[] settings = fileHandler.FileOutput(fullPath);
-                gameTag = settings[0];
-                darkmode = settings[1];
-                tbxGameTag.Text = settings[0];
-                if(settings[1] == "True")
+                InitializeComponent();
+                this.@fullPath = Path.Combine(_dirPath, _filename);
+                this.dirPath = _dirPath;
+                this.filename = _filename;
+
+                // Applies inital settings if present
+                bool dirExist = fileHandler.checkForDir(dirPath);
+                if(dirExist)
                 {
-                    cbxDM.Checked = true;
-                } else
-                {
-                    cbxDM.Checked = false;
+                    string[] settings = fileHandler.FileOutput(dirPath, filename);
+                    gameTag = settings[0];
+                    darkmode = settings[1];
+                    tbxGameTag.Text = settings[0];
+                    if(settings[1] == "True")
+                    {
+                        cbxDM.Checked = true;
+                    } else
+                    {
+                        cbxDM.Checked = false;
+                    }
                 }
+            } catch(Exception)
+            {
             }
         }
 
@@ -47,18 +52,18 @@ namespace Gomoku
         /// Gets settings
         /// </summary>
         /// <param name="_dirPath"></param>
-        public SettingsForm(string @_dirPath)
+        /*public SettingsForm(string @_dirPath)
         {
             // Applies inital settings if present
             bool dirExist = fileHandler.checkForDir(@_dirPath);
             System.Diagnostics.Debug.WriteLine(@_dirPath);
             if(dirExist)
             {
-                string[] settings = fileHandler.FileOutput(@_dirPath);
+                string[] settings = fileHandler.FileOutput(@_dirPath ,filename);
                 gameTag = settings[0];
                 darkmode = settings[1];
             }
-        }
+        }**/
 
         private void btnCancel_Click(object sender, EventArgs e)
         {
@@ -67,41 +72,51 @@ namespace Gomoku
 
         private void SettingsForm_FormClosed(object sender, FormClosedEventArgs e)
         {
-            DialogResult = DialogResult.Cancel;
         }
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            if(String.IsNullOrWhiteSpace(tbxGameTag.Text))
+            try
             {
-                gameTag = "";
-            } else
+                if(String.IsNullOrWhiteSpace(tbxGameTag.Text))
+                {
+                    gameTag = "";
+                } else
+                {
+                    gameTag = tbxGameTag.Text;
+                }
+                darkmode = cbxDM.Checked.ToString();
+                string[] settings = { gameTag, darkmode };
+
+                fileHandler.FileInput(settings, dirPath, filename);
+                System.Diagnostics.Debug.WriteLine("OK?");
+                DialogResult = DialogResult.OK;
+            } catch(Exception)
             {
-                gameTag = tbxGameTag.Text;
             }
-            darkmode = cbxDM.Checked.ToString();
-            string[] settings = { gameTag, darkmode };
-
-            fileHandler.FileInput(settings, dirPath, filename);
-
-            DialogResult = DialogResult.OK;
         }
 
         /// <summary>
         /// Gets settings
         /// </summary>
-        /// <param name="fullPath">The full paht including filename and extion</param>
+        /// <param name="dirPath">The directory paht exluding filename and extion</param>
+        /// <param name="filename">The filename including extion</param>
         /// <returns>Boolean: True if settings was found, false if not</returns>
-        public bool getSettings(string @fullPath)
+        public bool getSettings(string @dirPath, string filename)
         {
-            bool dirExist = fileHandler.checkForDir(fullPath);
-            if(dirExist)
+            try
             {
-                string[] settings = fileHandler.FileOutput(fullPath);
-                gameTag = settings[0];
-                darkmode = settings[1];
+                bool dirExist = fileHandler.checkForDir(dirPath);
+                if(dirExist)
+                {
+                    string[] settings = fileHandler.FileOutput(dirPath, filename);
+                    gameTag = settings[0];
+                    darkmode = settings[1];
 
-                return true;
+                    return true;
+                }
+            } catch(Exception)
+            {
             }
             return false;
         }
